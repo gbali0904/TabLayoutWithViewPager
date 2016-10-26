469db208635762c5a9cfd3e440d2b386d1584866
Gunjan <gbali0904@gmail.com>
2016-10-26 14:45:01 +0530

    Commit the change

---
+++ app/src/main/java/roy/tablayoutwithviewpager/MainActivity.java
@@ -0,0 +1,56 @@
+package roy.tablayoutwithviewpager;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ViewPager  pager;
+    TabLayout tabLayout;
+    private ImageView firstView;
+    private ImageButton leftNav;
+    private ImageButton rightNav;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pager= (ViewPager) findViewById(R.id.view_pager);
+
+        FragmentManager manager=getSupportFragmentManager();
+        PagerAdapter1 adapter=new PagerAdapter1(manager);
+        pager.setAdapter(adapter);
+        leftNav = (ImageButton) findViewById(R.id.left_nav);
+        rightNav = (ImageButton) findViewById(R.id.right_nav);
+        leftNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tab = pager.getCurrentItem();
+                if (tab > 0) {
+                    tab--;
+                    pager.setCurrentItem(tab);
+                } else if (tab == 0) {
+                    pager.setCurrentItem(tab);
+                }
+            }
+        });
+
+        // Images right navigatin
+        rightNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int tab = pager.getCurrentItem();
+                tab++;
+                pager.setCurrentItem(tab);
+            }
+        });
+    }
+
+  }
